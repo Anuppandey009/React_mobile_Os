a05a1fe90c6bbed0c30ee2a5db09adfd0e30bccf
Anuppandey009 <anuppandey000@gmail.com>
2021-10-20 01:26:53 +0530

    Mobile Operating System Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import {Manufacturers} from "./Components/Manufacturer"
+import {OperatingSystem} from "./Components/OperatingSystem"
+
+export default function App() {
+  return (
+  <div>
+   <Manufacturers/>
+   <OperatingSystem />
+   </div>
+  );
+}
